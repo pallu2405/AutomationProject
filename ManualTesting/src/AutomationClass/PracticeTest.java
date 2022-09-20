cfcb66e6c6ef28b48b3736e767ef676289ca3564
pallavi <pallavishinde05421@gmail.com>
2022-09-20 12:53:42 +0530

    Happy vibe

---
+++ ManualTesting/src/AutomationClass/PracticeTest.java
@@ -0,0 +1,11 @@
+package AutomationClass;
+
+public class PracticeTest {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("My name is Pallavi");
+		System.out.println("Pallavi Shinde");
+	}
+
+}
